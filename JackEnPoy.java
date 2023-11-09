74fc9bf435b53dc24f229a8a48d1b6942ecaf769
Jay Millena <jay.millena07@gmail.com>
2023-11-09 12:37:47 +0800

    Jack en poy

--- JackEnPoy.java
+++ JackEnPoy.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class JackEnPoy
 {
   public static void main(String[] args)
   {
     System.out.println("Welcome to Jack En Poy Game! ");
+    show_message();
+  }
+
+  public static void show_message()
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.print("What is your name? " );
+    String name = input.nextLine();
+    System.out.print("Hi "+ name);
   }
 }
